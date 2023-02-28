6f761686af48943e9888f2c9ffd88da1ac255721
haavardok <52386166+haavardok@users.noreply.github.com>
2023-02-28 22:49:43 +0100

    Finished problem 1a

---
+++ inspera_assignment_1/ContractDataBase.cpp
@@ -0,0 +1,91 @@
+#include "ContractDataBase.h"
+
+InsuranceContract ContractDataBase::getContract(int id)
+{
+    // BEGIN: 1b
+
+    // Write your answer to assignment 1b here, between the // BEGIN: 1b
+    // and // END: 1b comments. Remove the code that is already there.
+
+    return contracts.at(static_cast<size_t>(id));
+
+    // END: 1b
+
+    // Returning an empty contract if no contract with the given id is found.
+    return InsuranceContract{};
+}
+
+int ContractDataBase::numberOfInsuranceType(InsuranceType type)
+{
+    // BEGIN: 1c
+
+    // Write your answer to assignment 1c here, between the // BEGIN: 1c
+    // and // END: 1c comments. Remove the code that is already there.
+
+    (void)type;
+    return 0;
+
+    // END: 1c
+}
+
+int ContractDataBase::addContract(string holderName, InsuranceType insType, int value)
+{
+    // BEGIN: 1d 
+
+    // Write your answer to assignment 1d here, between the // BEGIN: 1d
+    // and // END: 1d comments. Remove the code that is already there.
+
+    (void)holderName;
+    (void)insType;
+    (void)value;
+    return 0;
+
+    // END: 1d
+}
+
+void ContractDataBase::saveContracts(string filename)
+{
+    // BEGIN: 1e
+
+    // Write your answer to assignment 1e here, between the // BEGIN: 1e
+    // and // END: 1e comments. Remove the code that is already there.
+
+    (void)filename;
+
+    // END: 1e
+}
+
+void ContractDataBase::loadContracts(string filename)
+{
+    ifstream inFile{filename};
+    if (!inFile)
+    {
+        error("Couldn't open file: ", filename);
+    }
+
+    string line;
+    while (getline(inFile, line))
+    {
+        stringstream ss{line};
+        char seperator;
+
+        string holderName;
+        getline(ss, holderName, ',');
+
+        string insType;
+        getline(ss, insType, ',');
+
+        int value;
+        ss >> value;
+        ss >> seperator;
+
+        int id;
+        ss >> id;
+        ss >> seperator;
+
+        string insuranceText;
+        getline(ss, insuranceText, ',');
+
+        contracts.push_back({holderName, stringToInsuranceType.at(insType), value, id, insuranceText});
+    }
+}
---
+++ inspera_assignment_1/Utilities.cpp
@@ -0,0 +1,39 @@
+#include "Utilities.h"
+
+string toGreek(string sentence)
+{
+    // BEGIN: 2a
+
+    // Write your answer to assignment 2a here, between the // BEGIN: 2a
+    // and // END: 2a comments. Remove the code that is already there.
+
+    (void)sentence;
+    return "";
+    
+    // END: 2a
+}
+
+vector<vector<string>> loadSvada()
+{
+    // BEGIN: 2b
+
+    // Write your answer to assignment 2b here, between the // BEGIN: 2b
+    // and // END: 2b comments. Remove the code that is already there.
+
+    return vector<vector<string>>{};
+
+    // END: 2b
+}
+
+string svadaGenerator(vector<vector<string>> svadaVec)
+{
+    // BEGIN: 2c1
+
+    // Write your answer to assignment 2c1 here, between the // BEGIN: 2c1
+    // and // END: 2c1 comments. Remove the code that is already there.
+
+    (void)svadaVec;
+    return "";
+    
+    // END: 2c1
+}
